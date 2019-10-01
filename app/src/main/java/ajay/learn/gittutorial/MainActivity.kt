80d077ce81b663d4aee4b1f75a053818091a2e9e
Ajayappnweb <ajay.rawat@appnweb.co.in>
2019-10-01 13:24:40 +0530

    inital commit

---
+++ app/src/main/java/ajay/learn/gittutorial/MainActivity.kt
@@ -0,0 +1,12 @@
+package ajay.learn.gittutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
